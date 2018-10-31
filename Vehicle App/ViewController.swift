79cb6212229c126f54da415e6d4251bf9d2651ca
cokeefe0319 <cokeefe0319@stu.d214.org>
2018-10-31 09:43:42 -0500

    Initial Commit

---
+++ Vehicle App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vehicle App
+//
+//  Created by Timothy P. Konopacki on 10/31/18.
+//  Copyright © 2018 TK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
